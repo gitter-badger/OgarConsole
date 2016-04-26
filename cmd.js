7afd007f60b16dc48a05c9da37083d0794c6db52
LegitSoulja <macballerboy@gmail.com>
2016-04-26 02:00:38 -0600

    Update cmd.js

--- cmd.js
+++ cmd.js
@@ -1,87 +1,80 @@
 // Console port.
 var serverport = 1000,
-advanced = false,
-// ./index is the start script.
-ogar = require('./index'),
-express = require("express"),
-Commands = require('./modules/CommandList'),
-app = express(),
-fs = require("fs"),
-server = require('http').createServer(app),
-io = require("socket.io").listen(server),
-GameServer = require("./GameServer"),
-exec = require('child_process').exec;
-
+    advanced = false,
+    // ./index is the start script.
+    ogar = require('./index'),
+    express = require("express"),
+    Commands = require('./modules/CommandList'),
+    app = express(),
+    fs = require("fs"),
+    server = require('http').createServer(app),
+    io = require("socket.io").listen(server),
+    GameServer = require("./GameServer"),
+    exec = require('child_process').exec;
 // Run Ogar
 var gameServer = new GameServer();
 var cache;
 gameServer.start(ogar.Version);
-
 // Add command handler
 gameServer.commands = Commands.list;
 module.exports = GameServer;
-
 // Listen for console
 server.listen(serverport);
-
 console.log("[Console] Console running port " + serverport);
-
-	// HTTP Listen.
-
-	app.get("/", function(req, res){
-		
-		fs.readFile(__dirname + "/cmd.html", function(err, data){
-			
-			res.send("" + data);
-			
-		
-		});
-		
-	});
-
-io.sockets.on("connection", function(socket){
-
-	socket.on("commandex", function(data){
-		
-		if(!advanced){
-			
-			try{
-				
-				gameServer.log.onCommand(data);
-				
-				if(data === "") { return; }
-				
-				var split = data.split(" ");
-				
-				var first = split[0].toLowerCase();
-				
-				var execute = gameServer.commands[first];
-				if(typeof execute != 'undefined'){
-					
-					execute(gameServer, split);
-					
-				}else{
-					console.log("[Console] Invalid Command, try \u001B[33mhelp\u001B[0m for a list of commands.");
-	  
-					socket.emit("input", "&#013;&#010;" + "[Console] Invalid Command, try \u001B[33mhelp\u001B[0m for a list of commands.");
-					
-				}
-				
-			}catch(e){
-				
-				console.log("[ERROR] Oh my, there seems to be an error with the command " + first);
-				console.log("[ERROR] Please alert AJS dev with this message:\n" + e);
-				socket.emit("input", "&#013;&#010;&#013;&#010" + "[ERROR] Oh my, there seems to be an error with the command " + first);
-				socket.emit("input", "&#013;&#010;&#013;&#010" + "[ERROR] Please alert AJS dev with this message:\n" + e);
-				
-			}
-			
-		}
-		
-		
-	});
-	
-	
+// HTTP Listen.
+app.get("/", function(req, res) {
+    fs.readFile(__dirname + "/cmd.html", function(err, data) {
+        res.send("" + data);
+    });
 });
-
-exec("title OgarConsole 1.0.0 / Port " + serverport, function(e, s, t){})
+var logThis;
+io.sockets.on("connection", function(socket) {
+    socket.on("commandex", function(data) {
+        if (!advanced) {
+            try {
+                gameServer.log.onCommand(data);
+                if (data === "") {
+                    return;
+                }
+                var split = data.split(" ");
+                var first = split[0].toLowerCase();
+                var execute = gameServer.commands[first];
+                if (typeof execute != 'undefined') {
+                    execute(gameServer, split);
+                    fs.readFile("./logs/console.log", function(
+                        err, data) {
+                        var a = data.toString();
+                        var clog = a.split("\n");
+                        for (var i in clog) {
+                            if (i < clog.length) {
+                                socket.emit("input",
+                                    clog[i]);
+                            }
+                        }
+                    })
+                } else {
+                    console.log(
+                        "[Console] Invalid Command, try 'help' for a list of commands."
+                    );
+                    socket.emit("input", "&#013;&#010;" +
+                        "[Console] Invalid Command, try 'help' for a list of commands."
+                    );
+                }
+            } catch (e) {
+                console.log(
+                    "[ERROR] Oh my, there seems to be an error with the command " +
+                    first);
+                console.log(
+                    "[ERROR] Please alert AJS dev with this message:\n" +
+                    e);
+                socket.emit("input", "&#013;&#010;&#013;&#010" +
+                    "[ERROR] Oh my, there seems to be an error with the command " +
+                    first);
+                socket.emit("input", "&#013;&#010;&#013;&#010" +
+                    "[ERROR] Please alert AJS dev with this message:\n" +
+                    e);
+            }
+        }
+    });
+});
+exec("title OgarConsole 1.0.0 / Port " + serverport, function(e, s, t) {})
